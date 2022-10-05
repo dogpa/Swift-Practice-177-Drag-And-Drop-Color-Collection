8fe87fbe5a6b0ae11f521fe95648c71f8ad9ac41
Dogpa <dogpa0130@gmail.com>
2022-10-05 14:37:37 +0800

    Initial Commit

---
+++ Swift Practice 177 Drag And Drop Color Collection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 177 Drag And Drop Color Collection
+//
+//  Created by Dogpa's MBAir M1 on 2022/10/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 177 Drag And Drop Color Collection/Swift_Practice_177_Drag_And_Drop_Color_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_177_Drag_And_Drop_Color_CollectionApp.swift
+//  Swift Practice 177 Drag And Drop Color Collection
+//
+//  Created by Dogpa's MBAir M1 on 2022/10/5.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_177_Drag_And_Drop_Color_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
